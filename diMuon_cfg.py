8b9d2c3333e891d3617dc82c98f9312c415933c5
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-07-12 20:22:49 +0200

    seperated diMuonPath

---
+++ diMuon_cfg.py
@@ -0,0 +1,61 @@
+import FWCore.ParameterSet.Config as cms
+import os,imp
+cfgFileTools = imp.load_source('module.name', os.getenv('CMSSW_BASE')+'/MyCMSSWAnalysisTools/Tools/cfgFileTools.py')
+debugCollection = cfgFileTools.debugCollection
+AddFilters = cfgFileTools.AddFilters
+
+###################
+def doDiMuonPath(process,pPF,debugIt = False):
+    if debugIt:
+      process.TFileService=cms.Service("TFileService",fileName=cms.string('patRefSel_diLep_cfg_debughistos.root'))
+      candPtHistogram = cms.PSet(min = cms.untracked.double(0), max = cms.untracked.double(400), nbins =  cms.untracked.int32 (200), name = cms.untracked.string('Pt'), description  = cms.untracked.string(''), plotquantity = cms.untracked.string('pt'))
+      candEtaHistogram = cms.PSet(min = cms.untracked.double(-5), max = cms.untracked.double(5), nbins =  cms.untracked.int32 (200), name = cms.untracked.string('Eta'), description  = cms.untracked.string(''), plotquantity = cms.untracked.string('eta'))
+    #################
+    Zmax=106;Zmin=76
+    diMuonTriggers = "HLT_DoubleMu6_v*"
+    process.myDiMuonPath = cms.Path(pPF._seq); localPath = process.myDiMuonPath
+    process.myDiMuonTriggerCheck = cms.EDFilter("TriggerResultsFilter",l1tIgnoreMask = cms.bool(False),l1tResults = cms.InputTag(""),l1techIgnorePrescales = cms.bool(False),    hltResults = cms.InputTag("TriggerResults","","HLT"),  triggerConditions = cms.vstring(diMuonTriggers),throw = cms.bool(False), daqPartitions = cms.uint32(1));  localPath += process.myDiMuonTriggerCheck;#analyzeColl("patMuonsPF",localPath,process,"diMuTriggerOK")
+    #my di muon selection
+    if debugIt:debugCollection('mySelectedPatMuons',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);debugCollection('mySelectedPatMuons2p1',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);
+    process.mySelectedPatMuonsMinCount = cms.EDFilter("PATCandViewCountFilter", maxNumber = cms.uint32(99), src = cms.InputTag("mySelectedPatMuons"), minNumber = cms.uint32(2))
+    process.mySelectedPatMuonsMaxCount = cms.EDFilter("PATCandViewCountFilter", maxNumber = cms.uint32(2), src = cms.InputTag("mySelectedPatMuons"), minNumber = cms.uint32(0))
+    process.mySelectedPatMuons2p1Count = cms.EDFilter("PATCandViewCountFilter", maxNumber = cms.uint32(2), src = cms.InputTag("mySelectedPatMuons2p1"), minNumber = cms.uint32(1))
+    localPath += process.mySelectedPatMuonsMinCount;localPath += process.mySelectedPatMuonsMaxCount;localPath += process.mySelectedPatMuons2p1Count
+    process.DiLepCandMuons = cms.EDProducer("DiLepCandProducer",srcColl1 = cms.InputTag("mySelectedPatMuons"), srcColl2 = cms.InputTag("mySelectedPatMuons"),cut = cms.string("mass > 20 "),pairCut = cms.string("(abs(mu1.pt - mu2.pt) > 0.1 || abs(mu1.px - mu2.px) > 0.1) && abs(mu1.charge - mu2.charge) > 0.1 &&  totalP4.M > 20")); localPath += process.DiLepCandMuons;
+    if debugIt:debugCollection('DiLepCandMuons',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);#analyzeColl("DiLepCandMuons",localPath,process,"DiLepCandOK");analyzeColl("mySelectedPatMuons",localPath,process,"MuonTest");
+    process.DiLepCandMuonsCount = cms.EDFilter("PATCandViewCountFilter", maxNumber = cms.uint32(99), src = cms.InputTag("DiLepCandMuons"), minNumber = cms.uint32(1));AddFilters.AddFilter(process.DiLepCandMuonsCount,localPath,process);#analyzeColl("DiLepCandMuons",localPath,process,"DiLepCandOK");analyzeColl("mySelectedPatMuons",localPath,process,"MuonTest");
+    #analyzeColl("DiLepCandMuons",localPath,process,"BOOMDiLepCandZVetoOK")
+    process.MuonsUsedForDiLepCand = cms.EDProducer("PATMuonCleaner",
+      src = cms.InputTag("mySelectedPatMuons"),
+      preselection = cms.string(''),# preselection (any string-based cut for pat::Muon)
+      checkOverlaps = cms.PSet(# overlap checking configurables
+          muons = cms.PSet(
+             src       = cms.InputTag("DiLepCandMuons"),
+             algorithm = cms.string("byDeltaR"),
+             preselection        = cms.string(""),  # don't preselect the muons
+             deltaR              = cms.double(9999),
+             checkRecoComponents = cms.bool(False), # don't check if they share some AOD object ref
+             pairCut             = cms.string('abs(cand1.pt-cand2.daughter("p1").pt)> 0.1 && abs(cand1.pt-cand2.daughter("p2").pt) > 0.1 '),
+             requireNoOverlaps   = cms.bool(True), # overlaps don't cause the electron to be discared
+          )
+      ),
+      finalCut = cms.string(''),# finalCut (any string-based cut for pat::Muon)
+    ); localPath += process.MuonsUsedForDiLepCand
+    if debugIt:debugCollection('MuonsUsedForDiLepCand',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);#analyzeColl("MuonsUsedForDiLepCand",localPath,process,"MuonUsedTest");
+    #analyzeColl("selectedPatJetsPF",localPath,process,"diMuontestSelPatJet")
+    #analyzeColl("MuonsUsedForDiLepCand",localPath,process,"diMuontestMuonsUsedForDiLepCand")
+    process.cleanJetsDiMuon = process.cleanPatJetsPF.clone();process.cleanJetsDiMuon.checkOverlaps = cms.PSet(    muons = cms.PSet(src = cms.InputTag("MuonsUsedForDiLepCand"),deltaR = cms.double(0.4),
+        pairCut = cms.string(''), checkRecoComponents = cms.bool(False),
+        algorithm = cms.string('byDeltaR'), preselection = cms.string(''), requireNoOverlaps = cms.bool(True))    );  localPath += process.cleanJetsDiMuon
+    #if debugIt:debugCollection('cleanJetsDiMuon',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);
+    process.DiLepCandMuonsZVeto = cms.EDFilter("PATCompositeCandidateRefSelector", src = cms.InputTag("DiLepCandMuons"), cut = cms.string("mass < "+str(Zmin) + " || mass > "+str(Zmax)) ); localPath += process.DiLepCandMuonsZVeto; 
+    #if debugIt:debugCollection('DiLepCandMuonsZVeto',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);
+    process.DiLepCandMuonsZVetoCount = cms.EDFilter("PATCandViewCountFilter", maxNumber = cms.uint32(99), src = cms.InputTag("DiLepCandMuonsZVeto"), minNumber = cms.uint32(1));AddFilters.AddFilter(process.DiLepCandMuonsZVetoCount,localPath,process);#analyzeColl("DiLepCandMuonsZVeto",localPath,process,"BOOMDiLepCandZVetoOK")
+    process.cleanJetsDiMuonCount =  cms.EDFilter("CandViewCountFilter", src = cms.InputTag("cleanJetsDiMuon"), minNumber = cms.uint32(2) ); AddFilters.AddFilter(process.cleanJetsDiMuonCount,localPath,process);
+    process.addBTagWeightsDiMu = cms.EDProducer("AddMyBTagWeights",jetSrc = cms.InputTag("cleanJetsDiMuon"));localPath += process.addBTagWeightsDiMu
+    if debugIt:debugCollection('cleanJetsDiMuon',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process,"DiMuJetMOk");#analyzeColl("cleanJetsDiMuon",localPath,process,"cleanJetsDiMuonJetMOK");analyzeColl("cleanJetsDiMuon",localPath,process,"JetMOK")
+    process.DiMuonmyselectedPatMETs = cms.EDFilter("PATMETSelector",  src = cms.InputTag("patMETsPF"),     cut = cms.string("energy > 40") ); localPath += process.DiMuonmyselectedPatMETs
+    #if debugIt:debugCollection('DiMuonmyselectedPatMETs',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);
+    process.DiMuonmyselectedPatMETsCount = cms.EDFilter("CandViewCountFilter", src = cms.InputTag("DiMuonmyselectedPatMETs"), minNumber = cms.uint32(1) ); AddFilters.AddFilter(process.DiMuonmyselectedPatMETsCount,localPath,process);#localPath += process.DiMuonmyselectedPatMETsCount
+    if debugIt:debugCollection('DiMuonmyselectedPatMETs',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process,"diMuonMETOK") 
+
--- patRefSel_diLep_cfg.py
+++ patRefSel_diLep_cfg.py
@@ -3,9 +3,13 @@
 # as defined in
 # https://twiki.cern.ch/twiki/bin/viewauth/CMS/TopLeptonPlusJetsRefSel_mu#Selection_Version_SelV4_valid_fr
 #
-executeDiElectronPath = True
+import imp,os
+executeDiElectronPath = False
+diMuon_cfg = imp.load_source('module.name', os.getenv('CMSSW_BASE')+'/DiLeptonicSelection/diMuon_cfg.py')
+cfgFileTools = imp.load_source('module.name', os.getenv('CMSSW_BASE')+'/MyCMSSWAnalysisTools/Tools/cfgFileTools.py')
+debugCollection = cfgFileTools.debugCollection
 executeDiMuonPath = True
-executeDiElectronMuonPath = True
+executeDiElectronMuonPath = False
 import sys
 from FWCore.ParameterSet.VarParsing import VarParsing
 options = VarParsing ('analysis')
@@ -23,21 +27,10 @@ def analyzeColl(coll,path,process,prefix=''):
  setattr(process,prefix+"test"+coll,newmod)
  path += getattr(process,prefix+"test"+coll)
 
-def debugCollection(coll,path,hists,process,prefix=""):
- newmod = cms.EDAnalyzer('CandViewHistoAnalyzer', src = cms.InputTag(coll), histograms = hists); setattr(process,prefix+coll+"TestAnalyzer",newmod); path += getattr(process,prefix+coll+"TestAnalyzer") 
-##########################
-class AddFilterAndCreatePath(object):
- def __init__(self,additionalPaths = True):
-  self.additionalPaths = additionalPaths
- def AddFilter(self,filter,path,process):
-  path += filter
-  if self.additionalPaths == True:
-   newPath = cms.Path(path._seq)
-   setattr(process,path.label()+"_"+filter.label()+"Path",newPath)
-#####################
+######################
 process = cms.Process( 'PAT' )
-AddFilters = AddFilterAndCreatePath(True)
-######
+AddFilters = cfgFileTools.AddFilters
+#######
 #dump cmsRun parameters
 print "this is cmsRun input: ",sys.argv
 
@@ -668,59 +661,14 @@ if runPF2PAT:
   pPF +=process.addPileupInfo
   from PhysicsTools.PatAlgos.tools.pfTools import *
   #adaptPFTaus( process, tauType='hpsPFTau', postfix=postfix )
-  Zmax=106;Zmin=76
   debugIt = True
   if debugIt:
    process.TFileService=cms.Service("TFileService",fileName=cms.string('patRefSel_diLep_cfg_debughistos.root'))
    candPtHistogram = cms.PSet(min = cms.untracked.double(0), max = cms.untracked.double(400), nbins =  cms.untracked.int32 (200), name = cms.untracked.string('Pt'), description  = cms.untracked.string(''), plotquantity = cms.untracked.string('pt'))
    candEtaHistogram = cms.PSet(min = cms.untracked.double(-5), max = cms.untracked.double(5), nbins =  cms.untracked.int32 (200), name = cms.untracked.string('Eta'), description  = cms.untracked.string(''), plotquantity = cms.untracked.string('eta'))
-  #executeDiMuonPath = True 
+  # DI Muon Signal 
   if executeDiMuonPath:
-    process.myDiMuonPath = cms.Path(pPF._seq); localPath = process.myDiMuonPath
-    process.myDiMuonTriggerCheck = cms.EDFilter("TriggerResultsFilter",l1tIgnoreMask = cms.bool(False),l1tResults = cms.InputTag(""),l1techIgnorePrescales = cms.bool(False),    hltResults = cms.InputTag("TriggerResults","","HLT"),  triggerConditions = cms.vstring(diMuonTriggers),throw = cms.bool(False), daqPartitions = cms.uint32(1));  localPath += process.myDiMuonTriggerCheck;#analyzeColl("patMuonsPF",localPath,process,"diMuTriggerOK")
-    #my di muon selection
-    if debugIt:debugCollection('mySelectedPatMuons',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);debugCollection('mySelectedPatMuons2p1',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);
-    process.mySelectedPatMuonsMinCount = cms.EDFilter("PATCandViewCountFilter", maxNumber = cms.uint32(99), src = cms.InputTag("mySelectedPatMuons"), minNumber = cms.uint32(2))
-    process.mySelectedPatMuonsMaxCount = cms.EDFilter("PATCandViewCountFilter", maxNumber = cms.uint32(2), src = cms.InputTag("mySelectedPatMuons"), minNumber = cms.uint32(0))
-    process.mySelectedPatMuons2p1Count = cms.EDFilter("PATCandViewCountFilter", maxNumber = cms.uint32(2), src = cms.InputTag("mySelectedPatMuons2p1"), minNumber = cms.uint32(1))
-    localPath += process.mySelectedPatMuonsMinCount;localPath += process.mySelectedPatMuonsMaxCount;localPath += process.mySelectedPatMuons2p1Count
-    process.DiLepCandMuons = cms.EDProducer("DiLepCandProducer",srcColl1 = cms.InputTag("mySelectedPatMuons"), srcColl2 = cms.InputTag("mySelectedPatMuons"),cut = cms.string("mass > 20 "),pairCut = cms.string("(abs(mu1.pt - mu2.pt) > 0.1 || abs(mu1.px - mu2.px) > 0.1) && abs(mu1.charge - mu2.charge) > 0.1 &&  totalP4.M > 20")); localPath += process.DiLepCandMuons;
-    if debugIt:debugCollection('DiLepCandMuons',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);#analyzeColl("DiLepCandMuons",localPath,process,"DiLepCandOK");analyzeColl("mySelectedPatMuons",localPath,process,"MuonTest");
-    process.DiLepCandMuonsCount = cms.EDFilter("PATCandViewCountFilter", maxNumber = cms.uint32(99), src = cms.InputTag("DiLepCandMuons"), minNumber = cms.uint32(1));AddFilters.AddFilter(process.DiLepCandMuonsCount,localPath,process);#analyzeColl("DiLepCandMuons",localPath,process,"DiLepCandOK");analyzeColl("mySelectedPatMuons",localPath,process,"MuonTest");
-    #analyzeColl("DiLepCandMuons",localPath,process,"BOOMDiLepCandZVetoOK")
-    process.MuonsUsedForDiLepCand = cms.EDProducer("PATMuonCleaner",
-      src = cms.InputTag("mySelectedPatMuons"),
-      preselection = cms.string(''),# preselection (any string-based cut for pat::Muon)
-      checkOverlaps = cms.PSet(# overlap checking configurables
-          muons = cms.PSet(
-             src       = cms.InputTag("DiLepCandMuons"),
-             algorithm = cms.string("byDeltaR"),
-             preselection        = cms.string(""),  # don't preselect the muons
-             deltaR              = cms.double(9999),
-             checkRecoComponents = cms.bool(False), # don't check if they share some AOD object ref
-             pairCut             = cms.string('abs(cand1.pt-cand2.daughter("p1").pt)> 0.1 && abs(cand1.pt-cand2.daughter("p2").pt) > 0.1 '),
-             requireNoOverlaps   = cms.bool(True), # overlaps don't cause the electron to be discared
-          )
-      ),
-      finalCut = cms.string(''),# finalCut (any string-based cut for pat::Muon)
-    ); localPath += process.MuonsUsedForDiLepCand
-    if debugIt:debugCollection('MuonsUsedForDiLepCand',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);#analyzeColl("MuonsUsedForDiLepCand",localPath,process,"MuonUsedTest");
-    #analyzeColl("selectedPatJetsPF",localPath,process,"diMuontestSelPatJet")
-    #analyzeColl("MuonsUsedForDiLepCand",localPath,process,"diMuontestMuonsUsedForDiLepCand")
-    process.cleanJetsDiMuon = process.cleanPatJetsPF.clone();process.cleanJetsDiMuon.checkOverlaps = cms.PSet(    muons = cms.PSet(src = cms.InputTag("MuonsUsedForDiLepCand"),deltaR = cms.double(0.4),
-        pairCut = cms.string(''), checkRecoComponents = cms.bool(False),
-        algorithm = cms.string('byDeltaR'), preselection = cms.string(''), requireNoOverlaps = cms.bool(True))    );  localPath += process.cleanJetsDiMuon
-    #if debugIt:debugCollection('cleanJetsDiMuon',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);
-    process.DiLepCandMuonsZVeto = cms.EDFilter("PATCompositeCandidateRefSelector", src = cms.InputTag("DiLepCandMuons"), cut = cms.string("mass < "+str(Zmin) + " || mass > "+str(Zmax)) ); localPath += process.DiLepCandMuonsZVeto; 
-    #if debugIt:debugCollection('DiLepCandMuonsZVeto',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);
-    process.DiLepCandMuonsZVetoCount = cms.EDFilter("PATCandViewCountFilter", maxNumber = cms.uint32(99), src = cms.InputTag("DiLepCandMuonsZVeto"), minNumber = cms.uint32(1));AddFilters.AddFilter(process.DiLepCandMuonsZVetoCount,localPath,process);#analyzeColl("DiLepCandMuonsZVeto",localPath,process,"BOOMDiLepCandZVetoOK")
-    process.cleanJetsDiMuonCount =  cms.EDFilter("CandViewCountFilter", src = cms.InputTag("cleanJetsDiMuon"), minNumber = cms.uint32(2) ); AddFilters.AddFilter(process.cleanJetsDiMuonCount,localPath,process);
-    process.addBTagWeightsDiMu = cms.EDProducer("AddMyBTagWeights",jetSrc = cms.InputTag("cleanJetsDiMuon"));localPath += process.addBTagWeightsDiMu
-    if debugIt:debugCollection('cleanJetsDiMuon',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process,"DiMuJetMOk");#analyzeColl("cleanJetsDiMuon",localPath,process,"cleanJetsDiMuonJetMOK");analyzeColl("cleanJetsDiMuon",localPath,process,"JetMOK")
-    process.DiMuonmyselectedPatMETs = cms.EDFilter("PATMETSelector",  src = cms.InputTag("patMETsPF"),     cut = cms.string("energy > 40") ); localPath += process.DiMuonmyselectedPatMETs
-    #if debugIt:debugCollection('DiMuonmyselectedPatMETs',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process);
-    process.DiMuonmyselectedPatMETsCount = cms.EDFilter("CandViewCountFilter", src = cms.InputTag("DiMuonmyselectedPatMETs"), minNumber = cms.uint32(1) ); AddFilters.AddFilter(process.DiMuonmyselectedPatMETsCount,localPath,process);#localPath += process.DiMuonmyselectedPatMETsCount
-    if debugIt:debugCollection('DiMuonmyselectedPatMETs',localPath,cms.VPSet(candPtHistogram,candEtaHistogram),process,"diMuonMETOK") 
+    diMuon_cfg.doDiMuonPath(process,pPF,True)
   #DI Electron Signal
   #executeDiElectronPath = True
   if executeDiElectronPath:
