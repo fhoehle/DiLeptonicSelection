deeefc79820111342a20c6429de718569ff52979
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-03-26 17:06:42 +0100

    added preFilterPath features

--- SelectionAnalysis/cutEfficiency.py
+++ SelectionAnalysis/cutEfficiency.py
@@ -1,5 +1,5 @@
 from DataFormats.FWLite import Events, Handle
-import commands,getopt,sys,os,argparse
+import commands,getopt,sys,os,argparse,re
 sys.path.extend([os.getenv('HOME')+'/PyRoot_Helpers/PyRoot_Functions',os.getenv('CMSSW_BASE')+'/MyCMSSWAnalysisTools'])
 
 #commandLine parsing
@@ -8,6 +8,7 @@ parser.add_argument('--input',required=True,help='input edm file, which TriggerR
 parser.add_argument('--showAvailablePaths',action='store_true',default=False,help='list available paths')
 parser.add_argument('--listTriggerResultCollections',action='store_true',default=False,help='show trigegrReport collections of input file')
 parser.add_argument('--processName',default='HLT',help='optional process name')
+parser.add_argument('--useMcSignal',default=False,action='store_true',help="calculate eff for signal mc events")
 args=parser.parse_args()
 print "called with ",sys.argv
 #
@@ -24,7 +25,15 @@ if args.listTriggerResultCollections:
     if m : 
       print "TriggerResults found in paths ",m.group(2),(" have sublabel "+m.group(1) if m.group(1) != "" else "")
   sys.exit(0)
-
+######
+interestingPaths = {
+  'isDiLepPath':{'pathName':'isDiLepPath','label':'diLepMC'}
+  ,'isDiMuonMcTagPath':{'pathName':'isDiMuonMcTagPath','label':'diLep_diMuonMC'}
+  ,'isDiElectronMcTagPath':{'pathName':'isDiElectronMcTagPath','label':'diLep_diElectronMC'}
+  ,'isMuonElectronMcTagPath':{'pathName':'isMuonElectronMcTagPath','label':'diLep_ElectronMuonMC'}
+  ,'cutFlowPath1myDiMuonPathgoodOfflinePrimaryVertices':{'pathName':'cutFlowPath1myDiMuonPathgoodOfflinePrimaryVertices','preFilterPath':'isDiMuonMcTagPath','label':'diMuon_goodVertex'}
+  }
+#################
 events = Events (args.input)
 #triggers
 TrigResultslabel=("TriggerResults");TrigResultshandle=Handle("edm::TriggerResults")
@@ -42,26 +51,23 @@ for i,event in enumerate(events):
       triggerEfficiencies[triggerName] = 0
     ###########
     if args.showAvailablePaths: sys.exit(0) 
-  
   for j in range(len(TriggerNames)):
     triggerName = TriggerNames.triggerName(j)
-    if TrigResults[TriggerNames.triggerIndex(triggerName)].accept():
+    mcTriggerName = "" if not interestingPaths.has_key(triggerName) or not args.useMcSignal else ( "" if not interestingPaths[triggerName].has_key('preFilterPath') else interestingPaths[triggerName]['preFilterPath'] )
+    mcSignal =  mcTriggerName == "" 
+    if not mcTriggerName == "": 
+      mcSignal = TrigResults[TriggerNames.triggerIndex(mcTriggerName)].accept()
+    if TrigResults[TriggerNames.triggerIndex(triggerName)].accept() and mcSignal:
       triggerEfficiencies[triggerName] += 1 
 ###############
 totalEvts=i+1
 import Tools.cfgFileTools as cfgFileTools
 print "===================================================="
 print "trigger efficiencies calculated using ",totalEvts," events"
-interestingPaths = {
-  'diLepMC':{'pathName':'isDiLepPath'}
-  ,'diLep_diMuonMC':{'pathName':'isDiMuonMcTagPath'}
-  ,'diLep_diElectronMC':{'pathName':'isDiElectronMcTagPath'}
-  ,'diLep_ElectronMuonMC':{'pathName':'isMuonElectronMcTagPath'}
-  }
 sortedTriggers = sorted(triggerEfficiencies.keys(),key=cfgFileTools.natural_sort_key)
 
 for trigN in interestingPaths.keys():
-  print trigN," "," events: ",triggerEfficiencies[interestingPaths[trigN]['pathName']]," eff. ",float(triggerEfficiencies[interestingPaths[trigN]['pathName']])/totalEvts
+  print interestingPaths[trigN]['label']," "," events: ",triggerEfficiencies[interestingPaths[trigN]['pathName']]," eff. ",float(triggerEfficiencies[interestingPaths[trigN]['pathName']])/(totalEvts if not interestingPaths[trigN].has_key('preFilterPath') or not args.useMcSignal else  triggerEfficiencies[interestingPaths[trigN]['preFilterPath']]), " ",trigN, ("" if not interestingPaths[trigN].has_key('preFilterPath') or not args.useMcSignal else " prePathFilter "+interestingPaths[trigN]['preFilterPath'])
 
 def printLatexTableLine(cut, eff, evts):
   return cut+" & "+eff+" & "+evts+" \\"
