f4599abe4a531f169e9ad4279cb5c0f7d2726deb
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-04-01 13:32:26 +0200

    interesting paths di muon and electron muon

--- SelectionAnalysis/cutEfficiency.py
+++ SelectionAnalysis/cutEfficiency.py
@@ -9,6 +9,7 @@ parser.add_argument('--showAvailablePaths',action='store_true',default=False,hel
 parser.add_argument('--listTriggerResultCollections',action='store_true',default=False,help='show trigegrReport collections of input file')
 parser.add_argument('--processName',default='HLT',help='optional process name')
 parser.add_argument('--useMCSignal',default=False,action='store_true',help="calculate eff for signal mc events")
+parser.add_argument('--showAllPathCutFlow',default='',help="show all paths for regex i.e. myDiMuonPath")
 args=parser.parse_args()
 print "called with ",sys.argv
 #
@@ -26,13 +27,34 @@ if args.listTriggerResultCollections:
       print "TriggerResults found in paths ",m.group(2),(" have sublabel "+m.group(1) if m.group(1) != "" else "")
   sys.exit(0)
 ######
-interestingPaths = {
-  'isDiLepPath':{'pathName':'isDiLepPath','label':'diLepMC'}
-  ,'isDiMuonMcTagPath':{'pathName':'isDiMuonMcTagPath','label':'diLep_diMuonMC'}
-  ,'isDiElectronMcTagPath':{'pathName':'isDiElectronMcTagPath','label':'diLep_diElectronMC'}
-  ,'isMuonElectronMcTagPath':{'pathName':'isMuonElectronMcTagPath','label':'diLep_ElectronMuonMC'}
-  ,'cutFlowPath1myDiMuonPathgoodOfflinePrimaryVertices':{'pathName':'cutFlowPath1myDiMuonPathgoodOfflinePrimaryVertices','preFilterPath':'isDiMuonMcTagPath','label':'diMuon_goodVertex'}
-  }
+import Tools.coreTools as coreTools
+coreTools.OrderedDict
+interestingPaths = coreTools.OrderedDict([
+  ('isDiLepPath',{'pathName':'isDiLepPath','label':'diLepMC'})
+  ,('isDiMuonMcTagPath',{'pathName':'isDiMuonMcTagPath','label':'diLep_diMuonMC'})
+  ,('isDiElectronMcTagPath',{'pathName':'isDiElectronMcTagPath','label':'diLep_diElectronMC'})
+  ,('isMuonElectronMcTagPath',{'pathName':'isMuonElectronMcTagPath','label':'diLep_ElectronMuonMC'})
+  #(##########################################
+  ,('cutFlowPath1myDiMuonPathgoodOfflinePrimaryVertices',{'pathName':'cutFlowPath1myDiMuonPathgoodOfflinePrimaryVertices','preFilterPath':'isDiMuonMcTagPath','label':'diMuon_goodVertex'})
+  ,('cutFlowPath35myDiMuonPathmyDiMuonTriggerCheck',{'pathName':'cutFlowPath35myDiMuonPathmyDiMuonTriggerCheck','preFilterPath':'isDiMuonMcTagPath','label':'diMuon_trigger'})
+  ,('cutFlowPath37myDiMuonPathmySelectedPatMuonsMaxCount',{'pathName':'cutFlowPath37myDiMuonPathmySelectedPatMuonsMaxCount','preFilterPath':'isDiMuonMcTagPath','label':'diMuon_oneMinAndMaxMuons'})
+  ,('cutFlowPath39myDiMuonPathDiLepCandMuonsCount',{'pathName':'cutFlowPath39myDiMuonPathDiLepCandMuonsCount','preFilterPath':'isDiMuonMcTagPath','label':'diMuon_DiLepCandMuons'})
+  ,('cutFlowPath41myDiMuonPathDiLepCandMuonsZVetoCount',{'pathName':'cutFlowPath41myDiMuonPathDiLepCandMuonsZVetoCount','preFilterPath':'isDiMuonMcTagPath','label':'diMuon_ZVeto'})
+  ,('cutFlowPath42myDiMuonPathcleanJetsDiMuonCount',{'pathName':'cutFlowPath42myDiMuonPathcleanJetsDiMuonCount','preFilterPath':'isDiMuonMcTagPath','label':'diMuon_jetCount'})
+  ,('cutFlowPath44myDiMuonPathDiMuonmyselectedPatMETsCount',{'pathName':'cutFlowPath44myDiMuonPathDiMuonmyselectedPatMETsCount','preFilterPath':'isDiMuonMcTagPath','label':'diMuon_MET'})
+  #(####################################
+  ,('cutFlowPath1myDiElectronMuonPathgoodOfflinePrimaryVertices',{'pathName':'cutFlowPath1myDiElectronMuonPathgoodOfflinePrimaryVertices','preFilterPath':'isMuonElectronMcTagPath','label':'diElectronMuon_goodVertex'})
+  ,('cutFlowPath35myDiElectronMuonPathmyMuonElectronTriggerCheck',{'pathName':'cutFlowPath35myDiElectronMuonPathmyMuonElectronTriggerCheck','preFilterPath':'isMuonElectronMcTagPath','label':'diElectronMuon_trigger'})
+  ,('cutFlowPath36myDiElectronMuonPathmySelectedPatElectronsMuonsMinCount',{'pathName':'cutFlowPath36myDiElectronMuonPathmySelectedPatElectronsMuonsMinCount','preFilterPath':'isMuonElectronMcTagPath','label':'diElectronMuon_eleMuonMin'})
+  ,('cutFlowPath37myDiElectronMuonPathmySelectedPatMuonsElectronsMinCount',{'pathName':'cutFlowPath37myDiElectronMuonPathmySelectedPatMuonsElectronsMinCount','preFilterPath':'isMuonElectronMcTagPath','label':'diElectronMuon_muonEleMin'})
+  ,('cutFlowPath38myDiElectronMuonPathDiLepCandEMuCount',{'pathName':'cutFlowPath38myDiElectronMuonPathDiLepCandEMuCount','preFilterPath':'isMuonElectronMcTagPath','label':'diElectronMuon_DiLepCand'})
+  ,('cutFlowPath39myDiElectronMuonPathMuonsUsedForDiLepCandEMuCount',{'pathName':'cutFlowPath39myDiElectronMuonPathMuonsUsedForDiLepCandEMuCount','preFilterPath':'isMuonElectronMcTagPath','label':'diElectronMuon_muonUsedForDiLepCandEMu'})
+  ,('cutFlowPath40myDiElectronMuonPathElectronsUsedForDiLepCandEMuCount',{'pathName':'cutFlowPath40myDiElectronMuonPathElectronsUsedForDiLepCandEMuCount','preFilterPath':'isMuonElectronMcTagPath','label':'diElectronMuon_eleUsedForDiLepCandEMu'})
+  ,('cutFlowPath41myDiElectronMuonPathcleanJetsDiEMuCount',{'pathName':'cutFlowPath41myDiElectronMuonPathcleanJetsDiEMuCount','preFilterPath':'isMuonElectronMcTagPath','label':'diElectronMuon_jetCount'})
+  ##############################################
+
+  ])
+
 #################
 events = Events (args.input)
 def getPathNames(evts,trigH,trigL):
@@ -49,7 +71,11 @@ triggerEfficiencies = {}
 TrigResultslabel=("TriggerResults");TrigResultshandle=Handle("edm::TriggerResults")
 triggerNamesList = getPathNames(events,TrigResultshandle,TrigResultslabel)
 ####
-
+if args.showAllPathCutFlow:
+  for n in triggerNamesList:
+    if interestingPaths.has_key(n):
+      continue
+    interestingPaths[n]= {'pathName':n,'label':'TEST'}
 ####
 for n in triggerNamesList:
   if args.showAvailablePaths:
@@ -75,9 +101,11 @@ totalEvts=i+1
 import Tools.cfgFileTools as cfgFileTools
 print "===================================================="
 print "trigger efficiencies calculated using ",totalEvts," events"
-sortedTriggers = sorted(triggerEfficiencies.keys(),key=cfgFileTools.natural_sort_key)
+sortedTriggers = interestingPaths.keys() if not args.showAllPathCutFlow else sorted([k for k in interestingPaths.keys() if args.showAllPathCutFlow in k],key=cfgFileTools.natural_sort_key)
+if args.showAllPathCutFlow:
+  print " only using paths containing ",args.showAllPathCutFlow
 
-for trigN in interestingPaths.keys():
+for trigN in sortedTriggers:#interestingPaths.keys():
   print interestingPaths[trigN]['label']," "," events: ",triggerEfficiencies[interestingPaths[trigN]['pathName']]," eff. ",float(triggerEfficiencies[interestingPaths[trigN]['pathName']])/(totalEvts if not interestingPaths[trigN].has_key('preFilterPath') or not args.useMCSignal else  triggerEfficiencies[interestingPaths[trigN]['preFilterPath']]), " ",trigN, ("" if not interestingPaths[trigN].has_key('preFilterPath') or not args.useMCSignal else " prePathFilter "+interestingPaths[trigN]['preFilterPath'])
 
 def printLatexTableLine(cut, eff, evts):
